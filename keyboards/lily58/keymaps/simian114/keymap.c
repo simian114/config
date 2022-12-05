d4a79b6de08f8c13521c0d9cb54e44f0a54cf483
sanam <simian114@stunning.kr>
2022-12-05 15:57:19 +0900

    chg: vimac scroll, hint mode map

--- keyboards/lily58/keymaps/simian114/keymap.c
+++ keyboards/lily58/keymaps/simian114/keymap.c
@@ -144,8 +144,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   GRAVE_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
   MY_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
   KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, HYPR(KC_F),  C(KC_O),  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  HYPR(KC_Y), \
-                KC_LALT, KC_LGUI, LOWER,      KC_SPC,         KC_ENT, RAISE, KC_BSPC, KC_RCTRL \
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, HYPR(KC_F),  HYPR(KC_Y),  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  HYPR(KC_F), \
+                KC_LALT, KC_LGUI, LOWER,      KC_SPC,         KC_ENT, RAISE, KC_BSPC, G(KC_SPC) \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
