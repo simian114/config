8b41b928bd0dd6f03ca64672108312b42a508b06
sanam <simian114@stunning.kr>
2023-08-15 22:07:08 +0900

    add: my chad custom config

---
+++ configs/null-ls.lua
@@ -0,0 +1,50 @@
+local null_ls = require "null-ls"
+
+local b = null_ls.builtins
+
+local sources = {
+
+  -- webdev stuff
+  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
+  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
+
+  -- Lua
+  b.formatting.stylua,
+
+  -- cpp
+  b.formatting.clang_format,
+}
+
+-- local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
+-- local event = "BufWritePre" -- or "BufWritePost"
+-- local async = event == "BufWritePost"
+
+-- local on_attach = function(client, bufnr)
+--   if client.supports_method "textDocument/formatting" then
+--     vim.keymap.set("n", "<Leader>f", function()
+--       vim.lsp.buf.format { bufnr = vim.api.nvim_get_current_buf() }
+--     end, { buffer = bufnr, desc = "[lsp] format" })
+--
+--     -- format on save
+--     vim.api.nvim_clear_autocmds { buffer = bufnr, group = group }
+--     vim.api.nvim_create_autocmd(event, {
+--       buffer = bufnr,
+--       group = group,
+--       callback = function()
+--         vim.lsp.buf.format { bufnr = bufnr, async = async }
+--       end,
+--       desc = "[lsp] format on save",
+--     })
+--   end
+--
+--   if client.supports_method "textDocument/rangeFormatting" then
+--     vim.keymap.set("x", "<Leader>f", function()
+--       vim.lsp.buf.format { bufnr = vim.api.nvim_get_current_buf() }
+--     end, { buffer = bufnr, desc = "[lsp] format" })
+--   end
+-- end
+
+null_ls.setup {
+  debug = true,
+  sources = sources,
+}
---
+++ mappings.lua
@@ -0,0 +1,96 @@
+---@type MappingsTable
+local M = {}
+
+M.general = {
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<leader>h"] = {
+      "<cmd> sb <CR>",
+      "Horizontal Split",
+    },
+    ["<leader>v"] = {
+      "<cmd> vert sb <CR>",
+      "Vertical Split",
+    },
+  },
+}
+
+-- more keybinds!
+
+M.nvimtree = {
+  n = {
+    ["<leader>e"] = {
+      function()
+        print(vim.bo.filetype)
+        if vim.bo.filetype == "NvimTree" then
+          vim.cmd.wincmd "p"
+        else
+          require("nvim-tree").focus "<CR>"
+        end
+      end,
+      desc = "toggle haha",
+    },
+  },
+}
+M.nvterm = {
+  plugin = true,
+
+  t = {
+    -- toggle in terminal mode
+    ["<leader>ti"] = {
+      function()
+        require("nvterm.terminal").toggle "float"
+      end,
+      "Toggle floating term",
+    },
+
+    ["<leader>th"] = {
+      function()
+        require("nvterm.terminal").toggle "horizontal"
+      end,
+      "Toggle horizontal term",
+    },
+
+    ["<leader>tv"] = {
+      function()
+        require("nvterm.terminal").toggle "vertical"
+      end,
+      "Toggle vertical term",
+    },
+  },
+
+  n = {
+    -- toggle in normal mode
+    ["<leader>ti"] = {
+      function()
+        require("nvterm.terminal").toggle "float"
+      end,
+      "Toggle floating term",
+    },
+
+    ["<leader>th"] = {
+      function()
+        require("nvterm.terminal").toggle "horizontal"
+      end,
+      "Toggle horizontal term",
+    },
+
+    ["<leader>tv"] = {
+      function()
+        require("nvterm.terminal").toggle "vertical"
+      end,
+      "Toggle vertical term",
+    },
+  },
+}
+
+M.telescope = {
+  plugin = true,
+
+  n = {
+    -- pick a hidden term
+    ["<leader>tt"] = { "<cmd> Telescope terms <CR>", "Pick hidden term" },
+  },
+}
+
+return M
