3ca5cdaeb8513e235baa852c7d976fb36b1338ee
sanam <simian114@stunning.kr>
2023-09-07 00:29:51 +0900

    add: nvim surround

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -144,6 +144,18 @@ local plugins = {
 
 	-- Neovim Tagbar
 	{ "preservim/tagbar" },
+
+	-- nvim surround
+	{
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
 	-- ==============================================================
 } -- end of plugin line
 
