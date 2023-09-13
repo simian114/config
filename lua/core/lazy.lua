8f8df48c0078248c757af0adc81f90dc8fd196fa
sanam <simian114@stunning.kr>
2023-09-13 22:41:42 +0900

    add: neoscroll

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -272,6 +272,12 @@ local plugins = {
 			end,
 		},
 		{ "famiu/bufdelete.nvim" },
+		{
+			"karb94/neoscroll.nvim",
+			config = function()
+				require("neoscroll").setup()
+			end,
+		},
 	},
 	-- ==============================================================
 	--
