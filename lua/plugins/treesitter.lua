1ab3cf8174701e025e4a539ef5204dd4a5ad266a
sanam <simian114@stunning.kr>
2023-11-05 01:12:27 +0900

    chg: treesitter incremental selecton

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -27,6 +27,15 @@ TreeSitter.setup({
 		enable = false,
 		additional_vim_regex_highlighting = false,
 	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "gnn", -- set to `false` to disable one of the mappings
+			node_incremental = "grn",
+			scope_incremental = "grc",
+			node_decremental = "grm",
+		},
+	},
 
 	autotag = {
 		enable = true,
