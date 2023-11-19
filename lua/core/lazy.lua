df7bc4075782d7be911f231660b126a1c9bcc65e
sanam <simian114@stunning.kr>
2023-11-19 12:35:11 +0900

    add: go.nvim

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -241,7 +241,20 @@ local plugins = {
 			require("lsp_signature").setup(opts)
 		end,
 	},
-	-- { "fatih/vim-go" },
+	{
+		"ray-x/go.nvim",
+		dependencies = { -- optional packages
+			"ray-x/guihua.lua",
+			"neovim/nvim-lspconfig",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("go").setup()
+		end,
+		event = { "CmdlineEnter" },
+		ft = { "go", "gomod" },
+		build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+	},
 	-- ==============================================================
 	--
 	--
